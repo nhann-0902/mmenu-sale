d03fbaac9bb21970e9c905d3d23729f40cd9d201
Nhânn <nhanhan3008@gmail.com>
2026-07-12 19:55:01 +0700

    Remove comment in fetch event listener
    Remove comment about avoiding cache interference.

--- sw.js
+++ sw.js
@@ -9,6 +9,5 @@ self.addEventListener('activate', (e) => {
 });
 
 self.addEventListener('fetch', (e) => {
-  // Trả về dữ liệu gốc, không can thiệp cache để tránh lỗi
   e.respondWith(fetch(e.request).catch(() => new Response("Vui lòng kết nối mạng để sử dụng MMENU.")));
 });
